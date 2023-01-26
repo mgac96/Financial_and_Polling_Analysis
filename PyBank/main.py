a31da6752d62bf8fa9ebeaa73245305b8f1bdf71
Molly Gac <mgac96@gmail.com>
2023-01-26 14:30:19 -0500

    revised python assingment

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,60 +2,58 @@ import os
 import csv
 #variable 
 total_profit = 0
-greatest_increase = ("mon-dd",0)
-greatest_decrease = ("mon-dd",0)
+greatest_increase = ["",0]
+greatest_decrease = ["",999999999]
 months = 0
 old = 0
 new = 0
 change = 0
 average_change = 0
 
-output = os.path.join("", "output.csv")
+output = os.path.join("","Analysis","output.txt")
 budget_csv = os.path.join("","Resources","budget_data.csv")
 
-with open (budget_csv,'r') as budget_csv:
-    csv_reader = csv.reader(budget_csv,delimiter=",")
-    csv_header = next(csv_reader)
-    
-    with open(output, 'w') as csvfile:
-        csvwriter = csv.writer(csvfile,delimiter=",")
-        csvwriter.writerow(csv_header)
-#months in dataset
-    for row in csv_reader:
-        months += 1
-#net profit
-    total_profit = total_profit + int(row[1])
-#change in profit loss
-    if old == 0:
-        csvwriter.writerow([row[0],row[1],0])
-        old = int(row[1])
-    else:
-        new = int(row[1])
-        change = new - old
-        csvwriter.writerow([row[0],row[1],change])
-        old = new
-        new = 0
-# greatest change in proft
-    if change < greatest_decrease[1]:
-        greatest_decrease[0] = row[0]
-        greatest_decrease[1] = change
-    if change > greatest_increase[1]:
-        greatest_increase[0] = row [0]
-        greatest_increase[1] = change
+with open (budget_csv) as budget_data:
+    reader = csv.reader(budget_data,delimiter=",")
+    header = next(reader)
+    for row in reader:
+    #months in dataset
+            months += 1
+    #net profit
+            total_profit = total_profit + int(row[1])
 
-#average change
+        #change in profit loss
+            if old == 0:
+               old = int(row[1])
+            else:
+                new = int(row[1])
+                change = new - old
+                average_change += change
+                old = new
+                new = 0
+    # greatest change in proft
+            if change < greatest_decrease[1]:
+                greatest_decrease[0] = row[0]
+                greatest_decrease[1] = change
+            if change > greatest_increase[1]:
+                greatest_increase[0] = row [0]
+                greatest_increase[1] = change
+
+    #average change
     average_change = round((average_change / (months -1)),2)
-#txt file
-    summaryfile = os.path.join("","Analysis","summary.txt")
-    with open(summaryfile,w) as f:
-        f.write("summary")
-        f.write(f"\nTotal months: {months}")
-        f.write(f"\nTotal profit : {total_profit}")
-        f.write(f"\nGreatestIncrease : {greatest_increase}")
-        f.write(f"\nGreatestDecrease : {greatest_decrease}")
-        f.write(f"\n Average change : {average_change}")
-    with open(summaryfile,'r') as text:
-        lines = text.read()
-        print(lines)
+    #txt file
+       
     
+    with open(output,"w")as textfile:       
+        summaryfile=(
+            f"Total months: {months}\n"
+            f"Total profit : {total_profit}\n"
+            f"GreatestIncrease : {greatest_increase}\n"
+            f"GreatestDecrease : {greatest_decrease}\n"
+            f"Average change : {average_change}")
+   
+        print(summaryfile,end="")
+        textfile.write(summaryfile)
+   
+        
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,30 +1,80 @@
 import os
 import csv
 
-election_csv = os.path.join("Resources","Election_Data.csv")
-total_votes = []
-with open(election_csv,'r') as election_csv:
-    csv_reader =csv.reader(election_csv,delimiter=",")
-    next (csv_reader)
-    row_count = 0
-    for row in csv_reader:
-            row_count += 1
-    total_votes = row_count
+election_csv = os.path.join("","Resources","Election_Data.csv")
 
-    stockham = 0
-    degette = 0
-    doane = 0
+output = os.path.join("","Analysis","output.txt")
 
-    for row in csv_reader:
-        if row[2] == "Charles Casper Stockham":
-            stockham += 1
-        elif row[2] == "Diana Degette":
-            degette +=1
-        elif row [2] =="Raymon Anthony Doane" :
-            doane += 1
+vote_total = 0
+vote_candidates = {}
+list_candidates = []
+winning_count = 0
+winning_candidates = ""
 
-    stockham_percent = stockham / total_votes
-    degette_percent = degette / total_votes
-    doane_percent = doane / total_votes
 
-    print("Election Results")
+with open(election_csv) as election_data:
+    reader = csv.reader(election_data)
+
+    header = next(reader)
+    
+    for row in reader: 
+
+        #print(".", end=""),
+    
+#The total number of votes cast
+
+        vote_total = vote_total + 1
+
+
+#A complete list of candidates who received votes
+
+        name_candidates = row[2]
+
+        # If the candidate does not match any existing candidate...
+        # (In a way, our loop is "discovering" candidates as it goes)
+        if name_candidates not in list_candidates:
+
+            # Add it to the list of candidates in the running
+            list_candidates.append(name_candidates)
+
+            # And begin tracking that candidate's voter count
+            vote_candidates[name_candidates] = 0
+
+        # Then add a vote to that candidate's count
+        vote_candidates[name_candidates] = vote_candidates[name_candidates] + 1
+
+with open(output, "w") as txtfile:
+#Loop through the counts
+
+    for candidate in vote_candidates:
+
+#The percentage of votes each candidate won
+        
+        votes = vote_candidates.get(candidate)
+        vote_percentage = float(votes) / float(vote_total) * 100
+
+#The winner of the election based on popular vote.
+
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+            
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        print(voter_output, end="")
+        txtfile.write(voter_output)
+            
+    winning_candidate_summary = (
+        f"-----------------------------------------\n"
+        f"Winner: {winning_candidate}\n")
+
+    print(winning_candidate_summary, end="")
+    txtfile.write(winning_candidate_summary)
+
+    election_results = (
+        f"Election Results: {winning_candidates}\n"
+        f"Total votes: {vote_total}\n"
+        f"-----------------------------------------\n")
+    print(election_results, end="")
+        #analysis_path = os.path.join("","Analysis","Analysis.txt")
+
+    txtfile.write(election_results)
